1828e495cab974aef253ee81b0b58976a696de54
vphilippoz <82140967+vphilippoz@users.noreply.github.com>
2025-03-25 11:28:15 +0100

    Created working backend module (without LED)

---
+++ src/backend/backend.hpp
@@ -0,0 +1,33 @@
+#ifndef BACKEND_H
+#define BACKEND_H
+
+#include <Arduino.h>
+
+namespace backend {
+    // Constants
+    constexpr unsigned int NUM_PUMPS = 4;       // Number of pumps
+    constexpr unsigned int NUM_LEDS = 1;        // Number of LEDs
+    constexpr unsigned int BRIGHTNESS = 100;    // LEDs brightness, 0-255
+
+    // Pinout
+    constexpr unsigned int PUMPS_PINS[] =   {4, 5, 6, 7};// Pins to control the water pumps
+    constexpr unsigned int LEDS_PIN =       8;          // Pin for onboard RGB LED
+
+    // Colors
+    // #define RED   CHSV(HSVHue::HUE_RED, 255, BRIGHTNESS)
+    // #define GREEN CHSV(HSVHue::HUE_GREEN, 255, BRIGHTNESS)
+    // #define BLUE  CHSV(HSVHue::HUE_BLUE, 255, BRIGHTNESS)
+    // #define YELLOW CHSV(HSVHue::HUE_YELLOW, 255, BRIGHTNESS)
+    // #define WHITE CHSV(0, 0, BRIGHTNESS)
+    // #define BLACK CHSV(0, 0, 0)
+
+    // Global variables declaration
+    extern bool pumps_active[];
+
+    // Function prototypes
+    void setup(void);
+    bool toggle_pump(unsigned int pump_ID);
+    
+} // namespace knobs
+
+#endif // BACKEND_H
---
+++ src/backend/backend.ino
@@ -0,0 +1,28 @@
+/*
+Minimal version to test the backend library.
+
+This code is a minimal version to test the backend library. It toggles the defined GPIOs and the corresponding LED color.
+*/
+
+#include <Arduino.h>
+#include "backend.hpp"
+
+// Constants
+constexpr unsigned int SERIAL_BAUD_RATE = 115200;
+
+
+void setup() {
+    // Open serial communications on the native USB port
+    Serial.begin(SERIAL_BAUD_RATE);while(!Serial);
+
+    // Setup the library
+    backend::setup();
+    Serial.println("Setup complete");
+}
+
+void loop() {
+    for(unsigned int i = 0; i < backend::NUM_PUMPS; i++) {
+        backend::toggle_pump(i);
+        delay(500);
+    }
+}
