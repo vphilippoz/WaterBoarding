fd82b13e1defd0fb28a08c804459253d862f2566
vphilippoz <82140967+vphilippoz@users.noreply.github.com>
2025-03-28 11:50:38 +0100

    LED unique color for any pump state

--- src/backend/backend.ino
+++ src/backend/backend.ino
@@ -19,8 +19,22 @@ void setup() {
 }
 
 void loop() {
+    // Toggle individually each pump
     for(unsigned int i = 0; i < backend::NUM_PUMPS; i++) {
         backend::toggle_pump(i);
-        delay(500);
+        delay(300);
+        backend::toggle_pump(i);
+        delay(100);
     }
+
+    delay(1000);
+
+    // Toggle all pumps
+    for(unsigned int i = 0; i < backend::NUM_PUMPS*2; i++) {
+        backend::toggle_pump(i%backend::NUM_PUMPS);
+        delay(200);
+    }
+
+    delay(1000);
+
 }
